c6606cdf056192c7670b8a627705428ad40bbe55
M. A. Alim Mukul <mukul.cse2010@gmail.com>
2022-09-20 23:23:29 +0200

    Initial Commit

---
+++ City_Sights/City_SightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_SightsApp.swift
+//  City_Sights
+//
+//  Created by M. A. Alim Mukul on 20.09.22.
+//
+
+import SwiftUI
+
+@main
+struct City_SightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
